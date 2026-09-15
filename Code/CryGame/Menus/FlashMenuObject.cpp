b28e8c820da6e65c25daff31170cec8b78738ccd
griseraner <griseraner@gmail.com>
2026-09-15 15:50:49 +0200

    Keep menu sound fadeout after OnLoadingComplete

--- Code/CryGame/Menus/FlashMenuObject.cpp
+++ Code/CryGame/Menus/FlashMenuObject.cpp
@@ -1194,7 +1194,7 @@ void CFlashMenuObject::OnLoadingComplete(ILevel* pLevel)
 	m_fMusicFirstTime = -1.0f;
 	if (m_pMusicSystem)
 	{
-		m_pMusicSystem->EndTheme(EThemeFade_StopAtOnce, 0, true);
+		m_pMusicSystem->EndTheme(EThemeFade_FadeOut, 0, true);
 		m_pMusicSystem->SetTheme("");
 	}
 
