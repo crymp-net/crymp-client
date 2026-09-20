8299db0b06449eff3e2632295470a21906b3d666
ccomrade <d.hryzbil@gmail.com>
2026-09-20 22:39:18 +0200

    Add vehicle CGA cache

--- Code/CryMP/Common/ServerPAK.h
+++ Code/CryMP/Common/ServerPAK.h
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <memory>
 #include <string>
+#include <vector>
 
-struct ILevelInfo;
+struct ICharacterInstance;
 
 class ServerPAK
 {
 	std::string m_path;
+
+	template<class T>
+	struct Releaser
+	{
+		void operator()(T* p) const { p->Release(); }
+	};
+
+	using SmartCharacterInstance = std::unique_ptr<ICharacterInstance, Releaser<ICharacterInstance>>;
+
+	std::vector<SmartCharacterInstance> m_cgaCache;
 
 public:
 	ServerPAK();
@@ -14,7 +26,10 @@ public:
 
 	bool Load(const std::string& path);
 	bool Unload();
-	void OnLoadingStart(ILevelInfo* pLevel);
+	void OnFirstLoadingProgress();
 	void OnDisconnect(int reason, const char* message);
 	void ResetSubSystems();
+
+private:
+	void ReloadCgaCache();
 };
